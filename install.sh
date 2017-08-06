3d8c7f23bfc85b9fe0240dbdf4b413a5fc6bad87
peondusud <reflexion_31@hotmail.com>
2017-08-07 00:02:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-apt-get update
-apt-get install build-essential libxml2-dev libxslt-dev python-dev python-lxml nodejs git
+   apt-get update 
+   apt-get install build-essential libxml2-dev libxslt-dev python3-dev python3-pip zlib1g-dev libffi-dev libssl-dev openjdk-8-jdk-headless git
 
-cd /tmp
-wget -c https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.3.3/elasticsearch-2.3.3.deb
-wget -c https://download.elastic.co/kibana/kibana/kibana_4.5.1_amd64.deb
-dpkg -i elasticsearch-2.3.3.deb kibana_4.5.1_amd64.deb
-pip install -U scrapy scrapyd scrapyd-client elasticsearch==2.3.0
-npm install -g scrapoxy
-
+   wget -c https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.5.1.deb
+   wget -c https://artifacts.elastic.co/downloads/kibana/kibana-5.5.1-amd64.deb
+   dpkg -i elasticsearch-5.5.1.deb kibana-5.5.1-amd64.deb
+   pip3 install -U scrapy scrapyd scrapyd-client elasticsearch==5.4.0

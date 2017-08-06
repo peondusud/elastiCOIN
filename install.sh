c0deff8c2a304acec97b9f0d4c0f8fd1c63aecc5
peondusud <reflexion_31@hotmail.com>
2017-08-07 00:29:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,33 @@
-   apt-get update 
-   apt-get install build-essential libxml2-dev libxslt-dev python3-dev python3-pip zlib1g-dev libffi-dev libssl-dev openjdk-8-jdk-headless git
-
-   wget -c https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.5.1.deb
-   wget -c https://artifacts.elastic.co/downloads/kibana/kibana-5.5.1-amd64.deb
-   dpkg -i elasticsearch-5.5.1.deb kibana-5.5.1-amd64.deb
-   pip3 install -U scrapy scrapyd scrapyd-client elasticsearch==5.4.0
-
+   #!/usr/bin/env bash
    
-   git clone https://github.com/peondusud/elastiCOIN.git
-   cd elastiCOIN/
-   systemctl restart elasticsearch
-   python3 elasticsearch/setup.py
+   BASEDIR=$(dirname "$0")
+   ES_VERSION=5.5.1
 
+   apt-get update
+   apt-get install -y git curl ntpdate
+   apt-get install build-essential libxml2-dev libxslt-dev python3-dev python3-pip zlib1g-dev libffi-dev libssl-dev openjdk-8-jdk-headless git
+   pip3 install -U setuptools scrapy scrapyd scrapyd-client elasticsearch==5.4.0
+   #npm install -g scrapoxy
+
+   #set timezone and local
+   echo "Europe/Paris" > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata
+   sed -i -e 's/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8' /etc/locale.gen
+   locale-gen
+   ntpdate 0.fr.pool.ntp.org
+
+   cd /tmp
+  
+   wget -c https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}.deb
+   dpkg -i elasticsearch-${ES_VERSION}.deb
+   systemctl restart elasticsearch
+   #push template
+   python3 ${BASEDIR}/elasticsearch/setup.py
+
+   wget -c https://artifacts.elastic.co/downloads/kibana/kibana-${ES_VERSION}-amd64.deb
+   dpkg -i kibana-${ES_VERSION}-amd64.deb
+   #listen from everywhere
+   sed -i 's|^#\?\(server.host:\).*|\1 "0.0.0.0"|g' /etc/kibana/kibana.yml
    systemctl restart kibana
-   cd scrapy/
+  
+   cd ${BASEDIR}/scrapy/
    echo -e "please type: \n scrapy crawl lbc or \n scrapy crawl lbc -a url=XXX"
